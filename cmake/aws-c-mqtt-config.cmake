88e031258b456ef2ab50c62735f4da1dbbbf2309
Jonathan M. Henson <henso@amazon.com>
2019-09-16 10:34:18 -0700

    Made shared/static libs work in the same build closure. (#85)

--- cmake/aws-c-mqtt-config.cmake
+++ cmake/aws-c-mqtt-config.cmake
@@ -6,4 +6,9 @@ if (@MQTT_WITH_WEBSOCKETS@)
     find_dependency(aws-c-http)
 endif()
 
-include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
+if (BUILD_SHARED_LIBS)
+    include(${CMAKE_CURRENT_LIST_DIR}/shared/@CMAKE_PROJECT_NAME@-targets.cmake)
+else()
+    include(${CMAKE_CURRENT_LIST_DIR}/static/@CMAKE_PROJECT_NAME@-targets.cmake)
+endif()
+
