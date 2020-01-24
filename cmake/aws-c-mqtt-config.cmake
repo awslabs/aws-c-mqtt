4d22db86b86bdabff4d9b2dbf57a2431734f74da
Jonathan M. Henson <henso@amazon.com>
2020-01-23 20:14:41 -0800

    Fix cmake config template. (#118)

--- cmake/aws-c-mqtt-config.cmake
+++ cmake/aws-c-mqtt-config.cmake
@@ -7,8 +7,8 @@ if (@MQTT_WITH_WEBSOCKETS@)
 endif()
 
 if (BUILD_SHARED_LIBS)
-    include(${CMAKE_CURRENT_LIST_DIR}/shared/@CMAKE_PROJECT_NAME@-targets.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
 else()
-    include(${CMAKE_CURRENT_LIST_DIR}/static/@CMAKE_PROJECT_NAME@-targets.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
 endif()
 
