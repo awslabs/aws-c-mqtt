f5ef29d4913ea30bfefacc2c58942437f70f4279
Dengke Tang <dengket@amazon.com>
2020-07-27 09:45:18 -0700

    clang format

--- source/client_channel_handler.c
+++ source/client_channel_handler.c
@@ -830,13 +830,14 @@ uint16_t mqtt_create_request(
     { /* BEGIN CRITICAL SECTION */
         s_mqtt_connection_lock_synced_data(connection);
 
-        if (connection->synced_data.state == AWS_MQTT_CLIENT_STATE_DISCONNECTING) {	
-            s_mqtt_connection_unlock_synced_data(connection);	
-            /* User requested disconnecting, ensure no new requests are made until the channel finished shutting down. */	
-            AWS_LOGF_ERROR(	
-                AWS_LS_MQTT_CLIENT, "id=%p: Disconnect requested, stop creating any new request.", (void *)connection);	
-            aws_raise_error(AWS_ERROR_MQTT_CONNECTION_DISCONNECTING);	
-            return 0;	
+        if (connection->synced_data.state == AWS_MQTT_CLIENT_STATE_DISCONNECTING) {
+            s_mqtt_connection_unlock_synced_data(connection);
+            /* User requested disconnecting, ensure no new requests are made until the channel finished shutting down.
+             */
+            AWS_LOGF_ERROR(
+                AWS_LS_MQTT_CLIENT, "id=%p: Disconnect requested, stop creating any new request.", (void *)connection);
+            aws_raise_error(AWS_ERROR_MQTT_CONNECTION_DISCONNECTING);
+            return 0;
         }
 
         next_request = aws_memory_pool_acquire(&connection->synced_data.requests_pool);
