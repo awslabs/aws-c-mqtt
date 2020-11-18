3e3efd5135975ebb1dfe47b1cba732f87500f998
Zach Giordano <70029738+giordzaws@users.noreply.github.com>
2020-11-18 12:41:14 -0800

    Mqtt test helper include fix (#164)
    * Fixing test helper include file.
    
    * Updating mqtt client test header with additional includes.

--- include/aws/mqtt/private/mqtt_client_test_helper.h
+++ include/aws/mqtt/private/mqtt_client_test_helper.h
@@ -5,21 +5,29 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-#include <aws/mqtt/private/client_impl.h>
+#include <aws/common/allocator.h>
+#include <aws/common/byte_buf.h>
+#include <aws/common/stdint.h>
+#include <aws/common/string.h>
+#include <aws/mqtt/exports.h>
 
 #ifndef AWS_UNSTABLE_TESTING_API
 #    error The functions in this header file are for testing purposes only!
 #endif
 
+struct aws_mqtt_client_connection;
+
 AWS_EXTERN_C_BEGIN
 
 /** This is for testing applications sending MQTT payloads. Don't ever include this file outside of a unit test. */
-AWS_MQTT_API void aws_mqtt_client_get_payload_for_outstanding_publish_packet(
+AWS_MQTT_API
+void aws_mqtt_client_get_payload_for_outstanding_publish_packet(
     struct aws_mqtt_client_connection *connection,
     uint16_t packet_id,
     struct aws_byte_cursor *result);
 
-AWS_MQTT_API void aws_mqtt_client_get_topic_for_outstanding_publish_packet(
+AWS_MQTT_API
+void aws_mqtt_client_get_topic_for_outstanding_publish_packet(
     struct aws_mqtt_client_connection *connection,
     uint16_t packet_id,
     struct aws_allocator *allocator,
