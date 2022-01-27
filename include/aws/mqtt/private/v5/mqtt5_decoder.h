c6680660655e7244df31998f073b83ce847837fe
Bret Ambrose <bretambrose@gmail.com>
2022-01-27 14:11:50 -0800

    Decode checkpoint to switch back and polish encode

---
+++ include/aws/mqtt/private/v5/mqtt5_decoder.h
@@ -0,0 +1,47 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#ifndef AWS_MQTT_MQTT5_DECODER_H
+#define AWS_MQTT_MQTT5_DECODER_H
+
+#include <aws/mqtt/mqtt.h>
+
+#include <aws/mqtt/v5/mqtt5_types.h>
+
+struct aws_mqtt5_client;
+
+enum aws_mqtt5_decoder_state {
+    AWS_MQTT5_DS_READ_PACKET_TYPE,
+    AWS_MQTT5_DS_READ_REMAINING_LENGTH,
+    AWS_MQTT5_DS_READ_PACKET,
+    AWS_MQTT5_DS_DECODE_PACKET,
+};
+
+struct aws_mqtt5_decoder_packet_state {
+    enum aws_mqtt5_packet_type type;
+    size_t total_length;
+    size_t remaining_length;
+};
+
+typedef int(
+    mqtt5_client_packet_callback_fn)(struct aws_mqtt5_client *client, enum aws_mqtt5_packet_type, void *packet_storage);
+typedef int(mqtt5_client_publish_payload_callback_fn)(
+    struct aws_mqtt5_client *client,
+    struct aws_mqtt5_packet_publish_view *publish_view,
+    struct aws_byte_cursor payload_bytes);
+
+struct aws_mqtt5_decoder {
+    struct aws_mqtt5_client *client;
+    mqtt5_client_packet_callback_fn *packet_callback;
+    mqtt5_client_publish_payload_callback_fn *publish_payload_callback;
+
+    enum aws_mqtt5_decoder_state state;
+    struct aws_mqtt5_decoder_packet_state packet_state;
+
+    struct aws_byte_buf pre_decode_buffer;
+    struct aws_byte_cursor pre_decode_cursor;
+};
+
+#endif /* AWS_MQTT_MQTT5_DECODER_H */
---
+++ source/v5/mqtt5_decoder.c
@@ -0,0 +1,6 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <aws/mqtt/private/v5/mqtt5_decoder.h>
