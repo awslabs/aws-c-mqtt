7a4ed5fadd76b78c570053a1a63c7014644dd5c9
Vera Xia <zhvxia@amazon.com>
2022-05-02 15:27:58 -0700

    fix errors in the script

--- codebuild/setup-libs.sh
+++ codebuild/setup-libs.sh
@@ -4,21 +4,18 @@ set -e
 
 env
 
-mkdir install
-INSTAL_PATH="/tmp/install"
+INSTALL_PATH="/tmp/install"
+mkdir $INSTALL_PATH
 
 function install_library {
     git clone https://github.com/awslabs/$1.git
-    pushd $1
-    mkdir build
 
     if [ -n "$2" ]; then
         git checkout $2
     fi
 
-    cmake -S $1 -B $1/build -DCMAKE_INSTALL_PREFIX=$INSTAL_PATH
+    cmake -S $1 -B $1/build -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH
     cmake --build $1/build --target install
-    popd
 }
 
 install_library aws-lc
@@ -30,12 +27,8 @@ install_library aws-c-compression
 install_library aws-c-http
 
 # build mqtt5 package
-cd $CODEBUILD_SRC_DIR
-mkdir build
-cmake -S $1 -B $CODEBUILD_SRC_DIR/build -DCMAKE_INSTALL_PREFIX=$INSTAL_PATH
+cmake -S $CODEBUILD_SRC_DIR -B $CODEBUILD_SRC_DIR/build -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH
 cmake --build $CODEBUILD_SRC_DIR/build --target install
-
-cd ..
 
 cert=$(aws secretsmanager get-secret-value --secret-id "unit-test/certificate" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$cert" > /tmp/certificate.pem
 key=$(aws secretsmanager get-secret-value --secret-id "unit-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$key" > /tmp/privatekey.pem
