b389707cb8148b2eb45aa440f4b8747f03a411c1
Vera Xia <zhvxia@amazon.com>
2022-05-02 15:13:09 -0700

    Revert "fix build script"
    This reverts commit cb10002671a252003ccd364646cef778709d38dd.
    
    Revert cb10002671a252003ccd364646cef778709d38dd

--- codebuild/setup-libs.sh
+++ codebuild/setup-libs.sh
@@ -4,11 +4,13 @@ set -e
 
 env
 
+mkdir install
 INSTAL_PATH="/tmp/install"
-mkdir $INSTAL_PATH
 
 function install_library {
     git clone https://github.com/awslabs/$1.git
+    pushd $1
+    mkdir build
 
     if [ -n "$2" ]; then
         git checkout $2
@@ -16,6 +18,7 @@ function install_library {
 
     cmake -S $1 -B $1/build -DCMAKE_INSTALL_PREFIX=$INSTAL_PATH
     cmake --build $1/build --target install
+    popd
 }
 
 install_library aws-lc
@@ -27,10 +30,13 @@ install_library aws-c-compression
 install_library aws-c-http
 
 # build mqtt5 package
-cmake -S $CODEBUILD_SRC_DIR -B $CODEBUILD_SRC_DIR/build -DCMAKE_INSTALL_PREFIX=$INSTAL_PATH
+cd $CODEBUILD_SRC_DIR
+mkdir build
+cmake -S $1 -B $CODEBUILD_SRC_DIR/build -DCMAKE_INSTALL_PREFIX=$INSTAL_PATH
 cmake --build $CODEBUILD_SRC_DIR/build --target install
-ctest $CODEBUILD_SRC_DIR/build
+
+cd ..
 
 cert=$(aws secretsmanager get-secret-value --secret-id "unit-test/certificate" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$cert" > /tmp/certificate.pem
 key=$(aws secretsmanager get-secret-value --secret-id "unit-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$key" > /tmp/privatekey.pem
-endpoint=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\")
+endpoint=$(aws secretsmanager get-secret-value --secret-id "unit-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\")
