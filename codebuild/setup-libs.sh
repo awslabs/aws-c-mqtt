cb10002671a252003ccd364646cef778709d38dd
Vera Xia <zhvxia@amazon.com>
2022-05-02 15:10:59 -0700

    fix build script

--- codebuild/setup-libs.sh
+++ codebuild/setup-libs.sh
@@ -4,13 +4,11 @@ set -e
 
 env
 
-mkdir install
 INSTAL_PATH="/tmp/install"
+mkdir $INSTAL_PATH
 
 function install_library {
     git clone https://github.com/awslabs/$1.git
-    pushd $1
-    mkdir build
 
     if [ -n "$2" ]; then
         git checkout $2
@@ -18,7 +16,6 @@ function install_library {
 
     cmake -S $1 -B $1/build -DCMAKE_INSTALL_PREFIX=$INSTAL_PATH
     cmake --build $1/build --target install
-    popd
 }
 
 install_library aws-lc
@@ -30,13 +27,10 @@ install_library aws-c-compression
 install_library aws-c-http
 
 # build mqtt5 package
-cd $CODEBUILD_SRC_DIR
-mkdir build
-cmake -S $1 -B $CODEBUILD_SRC_DIR/build -DCMAKE_INSTALL_PREFIX=$INSTAL_PATH
+cmake -S $CODEBUILD_SRC_DIR -B $CODEBUILD_SRC_DIR/build -DCMAKE_INSTALL_PREFIX=$INSTAL_PATH
 cmake --build $CODEBUILD_SRC_DIR/build --target install
-
-cd ..
+ctest $CODEBUILD_SRC_DIR/build
 
 cert=$(aws secretsmanager get-secret-value --secret-id "unit-test/certificate" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$cert" > /tmp/certificate.pem
 key=$(aws secretsmanager get-secret-value --secret-id "unit-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$key" > /tmp/privatekey.pem
-endpoint=$(aws secretsmanager get-secret-value --secret-id "unit-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\")
+endpoint=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\")
