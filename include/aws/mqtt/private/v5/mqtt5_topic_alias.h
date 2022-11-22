92ec233343745da5390f01d0507658c6d2856126
Bret Ambrose <bretambrose@gmail.com>
2022-11-22 13:01:11 -0800

    Proper import/export tags on private but test-used functions

--- include/aws/mqtt/private/v5/mqtt5_topic_alias.h
+++ include/aws/mqtt/private/v5/mqtt5_topic_alias.h
@@ -23,37 +23,39 @@ struct aws_mqtt5_inbound_topic_alias_resolver {
 
 AWS_EXTERN_C_BEGIN
 
-int aws_mqtt5_inbound_topic_alias_resolver_init(
+AWS_MQTT_API int aws_mqtt5_inbound_topic_alias_resolver_init(
     struct aws_mqtt5_inbound_topic_alias_resolver *resolver,
     struct aws_allocator *allocator);
 
-void aws_mqtt5_inbound_topic_alias_resolver_clean_up(struct aws_mqtt5_inbound_topic_alias_resolver *resolver);
+AWS_MQTT_API void aws_mqtt5_inbound_topic_alias_resolver_clean_up(
+    struct aws_mqtt5_inbound_topic_alias_resolver *resolver);
 
-int aws_mqtt5_inbound_topic_alias_resolver_reset(
+AWS_MQTT_API int aws_mqtt5_inbound_topic_alias_resolver_reset(
     struct aws_mqtt5_inbound_topic_alias_resolver *resolver,
     uint16_t cache_size);
 
-int aws_mqtt5_inbound_topic_alias_resolver_resolve_alias(
+AWS_MQTT_API int aws_mqtt5_inbound_topic_alias_resolver_resolve_alias(
     struct aws_mqtt5_inbound_topic_alias_resolver *resolver,
     uint16_t alias,
     struct aws_byte_cursor *topic_out);
 
-int aws_mqtt5_inbound_topic_alias_resolver_register_alias(
+AWS_MQTT_API int aws_mqtt5_inbound_topic_alias_resolver_register_alias(
     struct aws_mqtt5_inbound_topic_alias_resolver *resolver,
     uint16_t alias,
     struct aws_byte_cursor topic);
 
-struct aws_mqtt5_outbound_topic_alias_resolver *aws_mqtt5_outbound_topic_alias_resolver_new(
+AWS_MQTT_API struct aws_mqtt5_outbound_topic_alias_resolver *aws_mqtt5_outbound_topic_alias_resolver_new(
     struct aws_allocator *allocator,
     enum aws_mqtt5_client_outbound_topic_alias_behavior_type outbound_alias_behavior);
 
-void aws_mqtt5_outbound_topic_alias_resolver_destroy(struct aws_mqtt5_outbound_topic_alias_resolver *resolver);
+AWS_MQTT_API void aws_mqtt5_outbound_topic_alias_resolver_destroy(
+    struct aws_mqtt5_outbound_topic_alias_resolver *resolver);
 
-int aws_mqtt5_outbound_topic_alias_resolver_reset(
+AWS_MQTT_API int aws_mqtt5_outbound_topic_alias_resolver_reset(
     struct aws_mqtt5_outbound_topic_alias_resolver *resolver,
     uint16_t topic_alias_maximum);
 
-int aws_mqtt5_outbound_topic_alias_resolver_resolve_outbound_publish(
+AWS_MQTT_API int aws_mqtt5_outbound_topic_alias_resolver_resolve_outbound_publish(
     struct aws_mqtt5_outbound_topic_alias_resolver *resolver,
     const struct aws_mqtt5_packet_publish_view *publish_view,
     uint16_t *topic_alias_out,
