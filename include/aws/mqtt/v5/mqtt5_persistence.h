f81e318ff8197902bf858b9060487e351872fdf1
Bret Ambrose <bretambrose@gmail.com>
2023-01-11 08:34:58 -0800

    very little

---
+++ include/aws/mqtt/v5/mqtt5_persistence.h
@@ -0,0 +1,33 @@
+/**
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* SPDX-License-Identifier: Apache-2.0.
+*/
+
+#ifndef AWS_MQTT_MQTT5_PERSISTENCE_H
+#define AWS_MQTT_MQTT5_PERSISTENCE_H
+
+#include <aws/mqtt/mqtt.h>
+
+struct aws_mqtt5_persistence_handler;
+
+struct aws_mqtt5_persistence_handler_vtable {
+
+};
+
+struct aws_mqtt5_persistence_handler_noop_config {
+
+};
+
+struct aws_mqtt5_persistence_handler_memory_config {
+
+};
+
+struct aws_mqtt5_persistence_handler_file_config {
+
+};
+
+AWS_EXTERN_C_BEGIN
+
+AWS_EXTERN_C_END
+
+#endif /* AWS_C_MQTT_MQTT5_PERSISTENCE_H */
