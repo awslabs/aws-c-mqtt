ba11a74108f818b2b84b57b5ed276645adefe97e
Igor Abdrakhimov <igorabd@amazon.com>
2025-01-17 11:26:21 -0800

    Move subscriptions match

--- source/request-response/request_response_subscription_set.c
+++ source/request-response/request_response_subscription_set.c
@@ -8,7 +8,13 @@
 #include <aws/common/logging.h>
 #include <aws/mqtt/mqtt.h>
 
-void search(const struct aws_hash_table *subscriptions, const struct aws_byte_cursor *topic) {
+void aws_mqtt_request_response_client_subscriptions_destroy(struct aws_request_response_subscriptions *subscriptions) {
+    aws_hash_table_clean_up(&subscriptions->streaming_operation_subscription_lists);
+    aws_hash_table_clean_up(&subscriptions->streaming_operation_wildcards_subscription_lists);
+    aws_hash_table_clean_up(&subscriptions->request_response_paths);
+}
+
+void s_match_wildcard_subscriptions(const struct aws_hash_table *subscriptions, const struct aws_byte_cursor *topic) {
     AWS_LOGF_INFO(
         AWS_LS_MQTT_REQUEST_RESPONSE, "= Looking subscription for topic '" PRInSTR "'", AWS_BYTE_CURSOR_PRI(*topic));
 
@@ -63,5 +69,44 @@ void search(const struct aws_hash_table *subscriptions, const struct aws_byte_cu
         } else {
             AWS_LOGF_INFO(AWS_LS_MQTT_REQUEST_RESPONSE, "=== this is not the right subscription");
         }
+    }
+}
+
+void aws_mqtt_request_response_client_subscriptions_match(
+    const struct aws_request_response_subscriptions *subscriptions,
+    const struct aws_byte_cursor *topic,
+    aws_mqtt_stream_operation_subscription_match_fn *on_stream_operation_subscription_match,
+    aws_mqtt_request_operation_subscription_match_fn *on_request_operation_subscription_match,
+    const struct aws_protocol_adapter_incoming_publish_event *publish_event,
+    struct aws_mqtt_request_response_client *rr_client) {
+    /* Streaming operation handling */
+    struct aws_hash_element *subscription_filter_element = NULL;
+    if (aws_hash_table_find(
+            &subscriptions->streaming_operation_subscription_lists, topic, &subscription_filter_element) ==
+            AWS_OP_SUCCESS &&
+        subscription_filter_element != NULL) {
+        AWS_LOGF_DEBUG(
+            AWS_LS_MQTT_REQUEST_RESPONSE,
+            "id=%p: request-response client incoming publish on topic '" PRInSTR "' matches streaming topic",
+            (void *)rr_client,
+            AWS_BYTE_CURSOR_PRI(*topic));
+
+        on_stream_operation_subscription_match(subscription_filter_element->value, publish_event);
+    }
+
+    s_match_wildcard_subscriptions(&subscriptions->streaming_operation_wildcards_subscription_lists, topic);
+
+    /* Request-Response handling */
+    struct aws_hash_element *response_path_element = NULL;
+    if (aws_hash_table_find(&subscriptions->request_response_paths, &publish_event->topic, &response_path_element) ==
+            AWS_OP_SUCCESS &&
+        response_path_element != NULL) {
+        AWS_LOGF_DEBUG(
+            AWS_LS_MQTT_REQUEST_RESPONSE,
+            "id=%p: request-response client incoming publish on topic '" PRInSTR "' matches response path",
+            (void *)rr_client,
+            AWS_BYTE_CURSOR_PRI(publish_event->topic));
+
+        on_request_operation_subscription_match(rr_client, response_path_element->value, publish_event);
     }
 }
