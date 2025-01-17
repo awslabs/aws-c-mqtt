cbaaf986c66a0cebc833bd46b06e36fb6015e354
Igor Abdrakhimov <igorabd@amazon.com>
2025-01-17 11:26:21 -0800

    Move subscriptions match

--- include/aws/mqtt/private/request-response/request_response_subscription_set.h
+++ include/aws/mqtt/private/request-response/request_response_subscription_set.h
@@ -10,6 +10,28 @@
 #include <aws/common/hash_table.h>
 #include <aws/common/linked_list.h>
 #include <aws/mqtt/exports.h>
+#include <aws/mqtt/private/request-response/protocol_adapter.h>
+
+/* Holds subscriptions for request-response client. */
+struct aws_request_response_subscriptions {
+    /*
+     * Map from cursor (topic filter) -> list of streaming operations using that filter
+     */
+    struct aws_hash_table streaming_operation_subscription_lists;
+
+    /*
+     * Map from cursor (topic filter with wildcards) -> list of streaming operations using that filter
+     */
+    struct aws_hash_table streaming_operation_wildcards_subscription_lists;
+
+    /*
+     * Map from cursor (topic) -> request response path (topic, correlation token json path)
+     *
+     * We don't garbage collect this table over the course of normal client operation.  We only clean it up
+     * when the client is shutting down.
+     */
+    struct aws_hash_table request_response_paths;
+};
 
 /*
  * This is the (key and) value in hash table (4) above.
@@ -23,9 +45,30 @@ struct aws_rr_operation_list_topic_filter_entry {
     struct aws_linked_list operations;
 };
 
+struct aws_mqtt_request_response_client;
+struct aws_rr_response_path_entry;
+
+typedef void(aws_mqtt_stream_operation_subscription_match_fn)(
+    struct aws_rr_operation_list_topic_filter_entry *entry,
+    const struct aws_protocol_adapter_incoming_publish_event *publish_event);
+
+typedef void(aws_mqtt_request_operation_subscription_match_fn)(
+    struct aws_mqtt_request_response_client *rr_client,
+    struct aws_rr_response_path_entry *entry,
+    const struct aws_protocol_adapter_incoming_publish_event *publish_event);
+
 AWS_EXTERN_C_BEGIN
 
-AWS_MQTT_API void search(const struct aws_hash_table *subscriptions, const struct aws_byte_cursor *topic);
+AWS_MQTT_API void aws_mqtt_request_response_client_subscriptions_destroy(
+    struct aws_request_response_subscriptions *subscriptions);
+
+AWS_MQTT_API void aws_mqtt_request_response_client_subscriptions_match(
+    const struct aws_request_response_subscriptions *subscriptions,
+    const struct aws_byte_cursor *topic,
+    aws_mqtt_stream_operation_subscription_match_fn *on_stream_operation_subscription_match,
+    aws_mqtt_request_operation_subscription_match_fn *on_request_operation_subscription_match,
+    const struct aws_protocol_adapter_incoming_publish_event *publish_event,
+    struct aws_mqtt_request_response_client *rr_client);
 
 AWS_EXTERN_C_END
 
