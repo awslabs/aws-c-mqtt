56af1618b489c83178649d118e094c018ca7824f
Vera Xia <zhvxia@amazon.com>
2025-12-19 13:23:39 -0800

    fix aws_mqtt_append_sdk_metrics_to_username

--- source/mqtt_iot_sdk_metrics.c
+++ source/mqtt_iot_sdk_metrics.c
@@ -82,11 +82,14 @@ int aws_mqtt_append_sdk_metrics_to_username(
     }
 
     if (!metrics) {
-        if (output_username && aws_byte_buf_init(output_username, allocator, original_username->len)) {
+        if (output_username && aws_byte_buf_init(output_username, allocator, original_username->len) == AWS_OP_SUCCESS) {
             aws_byte_buf_write(output_username, original_username->ptr, original_username->len);
-            *out_full_username_size = original_username->len;
+            if (out_full_username_size){
+                *out_full_username_size = original_username->len;
+            } 
             return AWS_OP_SUCCESS;
         }
+        return AWS_OP_ERR;
     }
 
     if (aws_mqtt_validate_iot_sdk_metrics_utf8(metrics)) {
