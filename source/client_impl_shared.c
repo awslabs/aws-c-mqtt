9bd6ecb2083862cf1d65dbd1c07c5b4d8cb66386
Steve Kim <sbstevek@amazon.com>
2026-02-04 10:33:45 -0800

    the impl of the hashing support funcs

--- source/client_impl_shared.c
+++ source/client_impl_shared.c
@@ -217,6 +217,14 @@ bool aws_mqtt_compare_uint16_t_eq(const void *a, const void *b) {
     return *(uint16_t *)a == *(uint16_t *)b;
 }
 
+uint64_t aws_mqtt_hash_uint32_t(const void *item) {
+    return *(uint32_t *)item;
+}
+
+bool aws_mqtt_compare_uint32_t_eq(const void *a, const void *b) {
+    return *(uint32_t *)a == *(uint32_t *)b;
+}
+
 bool aws_mqtt_byte_cursor_hash_equality(const void *a, const void *b) {
     const struct aws_byte_cursor *a_cursor = a;
     const struct aws_byte_cursor *b_cursor = b;
