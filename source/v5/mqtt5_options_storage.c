5eb62f796416abf787ce93abf92a2446627f39c9
Vera Xia <zhvxia@amazon.com>
2026-02-16 13:30:07 -0800

    Add Metrics Support For Mqtt5-to-3 Adapter (#414)

--- source/v5/mqtt5_options_storage.c
+++ source/v5/mqtt5_options_storage.c
@@ -3424,7 +3424,7 @@ int aws_mqtt5_client_options_validate(const struct aws_mqtt5_client_options *opt
     if (options->metrics != NULL) {
         if (aws_mqtt_validate_iot_metrics(options->metrics)) {
             AWS_LOGF_ERROR(AWS_LS_MQTT5_GENERAL, "invalid metrics in mqtt5 client configuration");
-            return aws_raise_error(AWS_ERROR_MQTT5_CONNECT_OPTIONS_VALIDATION);
+            return aws_raise_error(AWS_ERROR_MQTT5_CLIENT_OPTIONS_VALIDATION);
         }
     }
 
